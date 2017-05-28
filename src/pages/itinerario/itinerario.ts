2a10d99eecc1aea19f4823b8df74cbb1688c9881
sergio <sergioj.sanabria@gmail.com>
2017-05-28 15:17:47 -0300

    RC5

--- src/pages/itinerario/itinerario.ts
+++ src/pages/itinerario/itinerario.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {ItinerarioConfirmadoPage} from "../itinerario-confirmado/itinerario-confirmado";
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { ItinerarioConfirmadoPage } from "../itinerario-confirmado/itinerario-confirmado";
 /**
  * Generated class for the ItinerarioPage page.
  *
@@ -14,15 +15,68 @@ import {ItinerarioConfirmadoPage} from "../itinerario-confirmado/itinerario-conf
 })
 export class ItinerarioPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loader: any;
+  constructor(public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    private geolocation: Geolocation,
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ItinerarioPage');
   }
 
-  irAConfirmacion(){
+  irAConfirmacion() {
     this.navCtrl.push(ItinerarioConfirmadoPage);
+  }
+
+  // funciones fijas
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Cargando...",
+    });
+    this.loader.present();
+  }
+
+  dismissLoading() {
+
+    console.log(this.loader);
+    if (this.loader) {
+      this.loader.dismiss();
+    }
+
+  }
+
+
+  comoLlegar() {
+    this.presentLoading();
+    this.geolocation.getCurrentPosition().then((resp) => {
+      let ways = [];
+
+      ways.push(resp.coords.latitude + ", " + resp.coords.longitude);
+
+      ways.push("-25.6056125 , -54.5655548");
+
+      ways.push("-25.6002357 , 	-54.5777312");
+
+      ways.push("-25.5993673 	-54.5656586");
+
+      ways.push("-25.6062552 	-54.567845");
+
+      let url = "https://www.google.com/maps/dir/" + ways.join('/');;
+
+      this.loader.dismissAll();
+
+      window.open(url, "_system");
+    }).catch((error) => {
+
+      this.loader.dismissAll();
+      console.log('Error getting location', error);
+      console.log(error);
+
+
+    });
+
   }
 
 }
